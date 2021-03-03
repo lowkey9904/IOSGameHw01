bcccb29e209b92a62e179b8de55737ade8e51622
lowkey9904 <00657140@email.ntou.edu.tw>
2021-03-03 11:41:24 +0800

    Initial Commit

---
+++ IOSGameHw01/IOSGameHw01App.swift
@@ -0,0 +1,17 @@
+//
+//  IOSGameHw01App.swift
+//  IOSGameHw01
+//
+//  Created by Joker on 2021/3/3.
+//
+
+import SwiftUI
+
+@main
+struct IOSGameHw01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
